70b6b506fd62e201b2ad62fb63f34098df492768
Aveek Saha <aveek.s98@gmail.com>
2022-10-27 17:05:06 -0400

    Create agent

---
+++ agent.py
@@ -0,0 +1,37 @@
+from dqn import *
+from collections import deque
+import random
+import numpy as np
+
+class Agent():
+
+    def __init__(self, state_size, action_size):
+        self.state_size = state_size
+        self.action_size = action_size
+        self.memory = deque(maxlen=5000)
+
+        self.model = DeepQNetwork(self.action_size, self.state_size)
+        self.target_model = DeepQNetwork(self.action_size, self.state_size)
+        self.target_model.set_weights(self.model.get_weights())
+
+    
+        self.gamma = 1.0
+        self.epsilon = 1.0
+        self.epsilon_min = 0.1
+        self.epsilon_decay = 0.995
+        self.update_rate = 1000
+
+    
+    def replay_memory(self, batch_size):
+        mini_batch = random.sample(self.memory, batch_size)
+        for state, action, reward, next_state, done in mini_batch:
+            target = reward
+            if not done:
+                target = (reward + self.gamma *
+                          np.amax(self.model.predict(next_state)[0]))
+
+            target_f = self.model.predict(state)
+            target_f[0][action] = target
+            self.model.fit(state, target_f, epochs=1, verbose=0)
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
